14a83f3071535a9d93be71c4197c1f48892b7501
hyzhao <hyzhao@telenavsoftware.com>
2023-10-31 13:51:15 +0800

    update code.

--- splash2/src/main/java/com/zhy/splash2/MainActivity.kt
+++ splash2/src/main/java/com/zhy/splash2/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.zhy.splash2
 
-import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
